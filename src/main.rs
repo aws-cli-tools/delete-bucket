043d64a1d5d18895e7205b43ec1fe918d1315bf2
Efi MK <efi.merdler@gmail.com>
2023-07-04 13:58:48 +0000

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,28 @@
 use anyhow::Result;
 use clap::Parser;
 use std::fmt::Debug;
-use whoami::{OutputType, StsClient};
+use delete_bucket::{OutputType, StsClient};
 
 #[derive(Debug, Parser)]
 struct Opt {
     /// The AWS Region.
-    #[clap(short, long)]
+    #[arg(short, long)]
     region: Option<String>,
 
     /// Which profile to use.
-    #[clap(short, long)]
+    #[arg(short, long)]
     profile: Option<String>,
 
+    /// Do not prompt for approval
+    #[arg(short, long)]
+    force: bool,
+
+    /// Bucket to delete 
+    #[arg(short, long)]
+    bucket: Vec<String>,
+
     #[arg(value_enum)]
-    #[arg(default_value_t=OutputType::String)]
-    #[clap(short, long)]
+    #[arg(short, long,default_value_t=OutputType::String)]
     output_type: OutputType,
 }
 
@@ -24,12 +31,12 @@ async fn main() -> Result<()> {
     env_logger::init();
     let args = Opt::parse();
 
-    let region_provider = whoami::get_region_provider(args.region);
+    let region_provider = delete_bucket::get_region_provider(args.region);
 
-    let shared_config = whoami::get_aws_config(args.profile, region_provider).await;
+    let shared_config = delete_bucket::get_aws_config(args.profile, region_provider).await;
 
     let client = StsClient::new(&shared_config);
-    whoami::get_caller_identity(&client, args.output_type, &mut std::io::stdout()).await?;
+    delete_bucket::get_caller_identity(&client, args.output_type, &mut std::io::stdout()).await?;
 
     Ok(())
 }
