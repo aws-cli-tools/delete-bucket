452baba5a6cd4ecd5a7c1958a961f8efcf8b5500
Serverless development in Hebrew <support@sharatless.cloud>
2023-07-16 19:42:07 +0300

    Improve error message (#4)

--- tests/cli.rs
+++ tests/cli.rs
@@ -53,7 +53,11 @@ mod cli_tests {
             .stdout(predicate::str::contains("deleted successfully."));
 
         let objects = client.list_objects_v2().bucket(&bucket_name).send().await;
-        assert!(objects.is_ok_and(|list| list.key_count() == 0));
+        // Sometimes AWS deletes the bucket, but when querying for it, it might return, so I'm checking both
+        // situations.
+        if let Ok(list) = objects {
+            assert!(list.key_count() == 0)
+        }
     }
 
     #[tokio::test]
