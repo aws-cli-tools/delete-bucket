f16ee3f6427d0de74c32b520b2fa160a02486182
Serverless development in Hebrew <support@sharatless.cloud>
2023-07-20 00:44:03 +0300

    delete versioned objects (#7)
    ---------
    
    Co-authored-by: Efi MK <efi.merdler@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,34 @@
 use anyhow::Result;
+use aws_sdk_s3::operation::list_object_versions::ListObjectVersionsError;
 use aws_sdk_s3::operation::list_objects_v2::{ListObjectsV2Error, ListObjectsV2Output};
-use aws_sdk_s3::types::{Delete, ObjectIdentifier};
+use aws_sdk_s3::types::{
+    BucketVersioningStatus, Delete, ObjectIdentifier, VersioningConfiguration,
+};
 use aws_sdk_s3::Client;
 use aws_sdk_sts::client::customize::Response;
 use aws_sdk_sts::error::SdkError;
-use console::{style, Emoji};
+use console::style;
 use futures::stream::FuturesUnordered;
 use indicatif::ProgressBar;
 use log::info;
 
 use tokio_stream::StreamExt;
 
-static LOOKING_GLASS: Emoji<'_, '_> = Emoji("🔍  ", "");
-static START_PROCESS: Emoji<'_, '_> = Emoji("🕛  ", "");
-static MIDDLE_PROCESS: Emoji<'_, '_> = Emoji("🕧  ", "");
-static END_PROCESS: Emoji<'_, '_> = Emoji("🕐  ", "");
-static SPARKLE: Emoji<'_, '_> = Emoji("✨ ", ":-)");
 const CHUNK_SIZE: usize = 1000;
 
+async fn disable_versioning(client: &Client, bucket_name: &str) {
+    // Suspend object versioning
+    let versioning_config = VersioningConfiguration::builder()
+        .set_status(Some(BucketVersioningStatus::Suspended))
+        .build();
+
+    let _ = client
+        .put_bucket_versioning()
+        .bucket(bucket_name)
+        .versioning_configuration(versioning_config)
+        .send()
+        .await;
+}
 async fn get_objects_to_delete(
     client: &Client,
     bucket_name: &str,
@@ -31,6 +42,92 @@ async fn get_objects_to_delete(
     paginator
         .collect::<Result<Vec<ListObjectsV2Output>, SdkError<ListObjectsV2Error, Response>>>()
         .await
+}
+
+async fn delete_versioned_objects(
+    client: &Client,
+    bucket_name: &str,
+) -> Result<(usize, usize), SdkError<ListObjectVersionsError>> {
+    info!("Calling 'delete_versioned_objects'");
+    let mut all_versions: Vec<(String, String)> = Vec::new();
+    let mut next_key_marker: Option<String> = None;
+    let mut next_version_id_marker: Option<String> = None;
+
+    // No paginator :-(
+    loop {
+        let mut request_builder = client.list_object_versions().bucket(bucket_name);
+
+        if let Some(marker) = next_key_marker {
+            request_builder = request_builder.key_marker(marker);
+        }
+
+        if let Some(marker) = next_version_id_marker {
+            request_builder = request_builder.version_id_marker(marker);
+        }
+
+        let result = request_builder.send().await?;
+
+        if let Some(versions) = result.versions() {
+            let tuples: Vec<(String, String)> = versions
+                .iter()
+                .filter_map(|version| {
+                    let version_id = version.version_id()?;
+                    let key = version.key()?;
+                    Some((version_id.to_string(), key.to_string()))
+                })
+                .collect();
+
+            all_versions.extend_from_slice(&tuples);
+        }
+        if let Some(versions) = result.delete_markers() {
+            let tuples: Vec<(String, String)> = versions
+                .iter()
+                .filter_map(|version| {
+                    let version_id = version.version_id()?;
+                    let key = version.key()?;
+                    Some((version_id.to_string(), key.to_string()))
+                })
+                .collect();
+
+            all_versions.extend_from_slice(&tuples);
+        }
+
+        if result.is_truncated() {
+            next_key_marker = result.next_key_marker().map(|s| s.to_string());
+            next_version_id_marker = result.next_version_id_marker().map(|s| s.to_string());
+        } else {
+            break;
+        }
+    }
+
+    let deleted_objects_count = all_versions.len();
+    let tasks = FuturesUnordered::new();
+    let pb = ProgressBar::new(deleted_objects_count as u64);
+
+    for item in all_versions {
+        let task = client
+            .delete_object()
+            .bucket(bucket_name)
+            .version_id(item.0)
+            .key(item.1)
+            .send();
+
+        let local_pb = pb.clone();
+        let wrapped_task = async move {
+            let result = task.await;
+            local_pb.inc(1);
+            result
+        };
+
+        tasks.push(wrapped_task);
+    }
+
+    let results = tasks.collect::<Vec<_>>().await;
+
+    let successful_count = results.iter().filter(|&result| result.is_ok()).count();
+    let failed_count = deleted_objects_count - successful_count;
+
+    Ok((successful_count, failed_count))
 }
 
 async fn delete_objects(
@@ -89,9 +186,16 @@ pub async fn delete_bucket(
 ) -> Result<()> {
     writeln!(
         writer,
-        "{} {}Collecting objects to delete",
-        style("[1/5]").bold().dim(),
-        LOOKING_GLASS
+        "{} Disabling Bucket Versioning",
+        style("[1/7]").bold().dim(),
+    )?;
+
+    disable_versioning(client, bucket_name).await;
+
+    writeln!(
+        writer,
+        "{} Collecting objects to delete",
+        style("[2/7]").bold().dim(),
     )?;
 
     let result = get_objects_to_delete(client, bucket_name).await;
@@ -110,33 +214,71 @@ pub async fn delete_bucket(
     }
     writeln!(
         writer,
-        "{} {}Deleting {} objects ...",
-        style("[2/5]").bold().dim(),
-        START_PROCESS,
+        "{} Deleting {} objects ...",
+        style("[3/7]").bold().dim(),
         counter
     )?;
     let deleted_objects_count = delete_objects(client, bucket_name, &objects).await?;
     writeln!(
         writer,
-        "{} {}Successfully deleted {} objects.",
-        style("[3/5]").bold().dim(),
-        MIDDLE_PROCESS,
+        "{} Successfully deleted {} objects.",
+        style("[4/7]").bold().dim(),
         deleted_objects_count
     )?;
 
+    if deleted_objects_count < counter as usize {
+        info!("Failed deleting all objects.");
+        return Err(anyhow::anyhow!("Failed deleting all object."));
+    }
+
     writeln!(
         writer,
-        "{} {}Deleting the bucket {}.",
-        style("[4/5]").bold().dim(),
-        END_PROCESS,
+        "{} Deleting object versions ...",
+        style("[5/7]").bold().dim(),
+    )?;
+    let (successful_count, failed_count) = match delete_versioned_objects(client, bucket_name).await
+    {
+        Ok((successful_count, failed_count)) => (successful_count, failed_count),
+        Err(err) => {
+            let service_error = err.into_service_error();
+            info!("Call failed {:?}", service_error);
+            return Err(anyhow::anyhow!("{}", service_error.meta().code().unwrap()));
+        }
+    };
+    let failed_text = if failed_count > 0 {
+        format!("Failed deleting {} versioned objects", failed_count)
+    } else {
+        String::from("")
+    };
+    writeln!(
+        writer,
+        "{} Successfully deleted {} object versions. {}",
+        style("[6/7]").bold().dim(),
+        successful_count,
+        failed_text
+    )?;
+
+    if failed_count > 0 {
+        info!("Failed deleting all objects.");
+        return Err(anyhow::anyhow!("Failed deleting all object versions."));
+    }
+
+    writeln!(
+        writer,
+        "{} Deleting the bucket {}.",
+        style("[7/7]").bold().dim(),
         style(bucket_name).white()
     )?;
-    client.delete_bucket().bucket(bucket_name).send().await?;
+    if let Err(err) = client.delete_bucket().bucket(bucket_name).send().await {
+        let service_error = err.into_service_error();
+        info!("Call failed {:?}", service_error);
+        return Err(anyhow::anyhow!("{}", service_error.meta().code().unwrap()));
+    }
+
     writeln!(
         writer,
-        "{} {}Bucket {} deleted successfully.",
-        style("[5/5]").bold().dim(),
-        SPARKLE,
+        "{} Bucket {} deleted successfully.",
+        style("[6/6]").bold().dim(),
         style(bucket_name).white()
     )?;
 
